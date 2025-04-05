0acb569b939c3a2b7b6f373e7c2ebd36a0448a7d
Leander Cretegny <leander.cretegny@epfl.ch>
2025-04-05 22:07:17 +0200

    forgot script

--- TheForgottenGrimoire/Assets/scripts/Staff.cs
+++ TheForgottenGrimoire/Assets/scripts/Staff.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class Staff : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _fireball;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
