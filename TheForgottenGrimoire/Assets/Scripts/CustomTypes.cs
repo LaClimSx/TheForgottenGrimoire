8eff967877995b665b91ad5470beab136ac1b8a2
Mai-LinhC <mai-linh.cordonnier@epfl.ch>
2025-04-16 16:18:17 +0200

    create customTypes : ElementType and SpellForm

---
+++ TheForgottenGrimoire/Assets/Scripts/CustomTypes.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CustomTypes
+{
+    public enum ElementType {
+        Earth,
+        Wind,
+        Fire,
+        Elec,
+        Space
+
+    }
+
+    public enum SpellForm {
+        Fist,
+        Pinch,
+        Open,
+    }
+
+}
