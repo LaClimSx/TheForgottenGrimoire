676d42bb7d215d02248836078b07f460825a98f4
Antoine Cornaz <antoine.cornaz01@gmail.com>
2025-05-25 18:44:50 +0200

    add earth hub and page to unlock spells

--- TheForgottenGrimoire/Assets/Scripts/UnlockSpell.cs
+++ TheForgottenGrimoire/Assets/Scripts/UnlockSpell.cs
@@ -1,16 +1,22 @@
+using Spells;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+[RequireComponent(typeof(BoxCollider))]
 public class UnlockSpell : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private SpellManager xrSpellCaster;
+    [SerializeField] private SpellType[] spellTypes;
+    
+    private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("trigger enter");
+        foreach (var type in spellTypes)
+        {
+            Debug.Log($"{type} unlocked");
+            xrSpellCaster.UnlockSpell(type);
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
